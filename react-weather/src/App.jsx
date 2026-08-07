cc96e7ae37f335ce3b0a4b52fb2ebb032b510071
SvyatoslavKys <svyat.ne@gmail.com>
2026-08-07 13:20:42 +0200

    addedBG

--- react-weather/src/App.jsx
+++ react-weather/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header'
 import { WeatherBackground } from './components/WeatherBackground'
 import { WeatherPage } from './pages/WeatherPage'
@@ -10,9 +10,9 @@ import { MoonCalendar } from './pages/MoonCalendar'
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <div className="relative min-h-screen overflow-hidden text-slate-950">
+      <>
         <WeatherBackground />
-        <div className="relative z-10 min-h-screen">
+        <div className="app-shell relative z-10 overflow-hidden text-slate-950">
           <Header />
           <main className="mx-auto max-w-6xl px-4 pb-8 pt-4 sm:px-5 sm:pb-10 sm:pt-5">
             <Routes>
@@ -20,10 +20,11 @@ function App() {
               <Route path="/weather" element={<WeatherPage />} />
               <Route path="/app" element={<AppPage />} />
               <Route path="/moon" element={<MoonCalendar />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
         </div>
-      </div>
+      </>
     </BrowserRouter>
   )
 }
